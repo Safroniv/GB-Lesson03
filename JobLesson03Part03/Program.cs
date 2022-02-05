3efdabd7a6ca123d2d46badc8e268e0103609bdc
Safroniv <safroniv@mail.ru>
2022-02-05 18:50:33 +0300

    Урок 3 Задание 3

--- JobLesson03Part03/Program.cs
+++ JobLesson03Part03/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, выводящую введённую пользователем строку в обратном порядке
+//(olleH вместо Hello).
+//string userString = Convert.ToString(Console.ReadLine());
+
+
+string stringReverse = Console.ReadLine();
+//разворот строки через reverce:
+static string ReverseString(string stringToReverce)
+{
+    char[] charsOfString = stringToReverce.ToCharArray();
+    Array.Reverse(charsOfString);
+    return new String(charsOfString);
+}
+Console.WriteLine(Convert.ToString(ReverseString(stringReverse)));
+Console.WriteLine();
+
+//Разворот строки через for:
+
+char[] chars = stringReverse.ToCharArray();
+
+for (int i = chars.Length; i > 0; i--)
+{
+    Console.Write(chars[i-1]);
+}
+Console.ReadLine();
